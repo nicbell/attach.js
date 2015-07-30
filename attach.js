73e3e8d7f50f6a328d99b5b1012a1e5a460e86cd
Nic Bell <nibell@mbugri1.tmw.local>
2015-07-30 14:06:52 +0100

    Added AMD, CommonJS and Node module support bumped version to 1.0.4

--- attach.js
+++ attach.js
@@ -1,45 +1,66 @@
 /**
- * Attach.js v1.0.3
+ * Attach.js v1.0.4
  * Attaches JavaScript to HTML without messy selectors. 
  *
  * https://github.com/nicbell/attach.js
  * MIT License
- * Copyright 2013 Nic Bell
+ * Copyright 2015 Nic Bell
  */
 
-;(function (Attach) {
-    Attach.engine = null;
+(function (global) {
+    'use strict';
 
-    Attach.items = [];
+    var Attach = {
+        engine: null,
+        items: [],
 
-    Attach.add = function (id, fn) {
-        this.items.push({ id: id, func: fn });
-    };
+        add: function (id, fn) {
+            this.items.push({ id: id, func: fn });
+        },
 
-    Attach.remove = function (id) {
-        for (var i = this.items.length; i--;) {
-            if (this.items[i].id === id) {
-                this.items.splice(i, 1);
-                break;
+        remove: function (id) {
+            for (var i = this.items.length; i--;) {
+                if (this.items[i].id === id) {
+                    this.items.splice(i, 1);
+                    break;
+                }
+            }
+        },
+
+        run: function () {
+            var elements = this.engine ? this.engine('[data-attach]') : document.querySelectorAll('[data-attach]');
+
+            for (var i = 0; i < elements.length; i++) {
+                this._attach(elements[i]);
+            }
+        },
+
+        _attach: function (element) {
+            var ids = element.getAttribute('data-attach').split(' ');
+
+            for (var i = 0; i < this.items.length; i++) {
+                if (ids.indexOf(this.items[i].id) >= 0 && typeof this.items[i].func === 'function') {
+                    this.items[i].func.call(this, element);
+                }
             }
         }
     };
 
-    Attach.run = function () {
-        var elements = this.engine ? this.engine('[data-attach]') : document.querySelectorAll('[data-attach]');
 
-        for (var i = 0; i < elements.length; i++) {
-            this._attach(elements[i]);
+    // AMD support
+    if (typeof define === 'function' && define.amd) {
+        define(function () { return Attach; });
+    }
+    else if (typeof exports !== 'undefined') {
+        // Support Node.js specific 'module.exports' (which can be a function)
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = Attach;
         }
-    };
+        // But always support CommonJS module 1.1.1 spec ('exports' cannot be a function)
+        exports.Attach = Attach;
+    }
+    else {
+        global.Attach = Attach;
+    }
 
-    Attach._attach = function (element) {
-        var ids = element.getAttribute('data-attach').split(' ');
-
-        for (var i = 0; i < this.items.length; i++) {
-            if (ids.indexOf(this.items[i].id) >= 0 && typeof this.items[i].func === 'function') {
-                this.items[i].func.call(this, element);
-            }
-        }
-    };
-})(window.Attach = window.Attach || {});
+})(this);
